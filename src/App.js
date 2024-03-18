2509d693022a2959a5db092a646f66c052f240e9
You <xin.yang02@mihoyo.com>
2024-03-18 11:36:54 +0800

    feat: use classnames

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import classNames from "classnames";
+import { useState } from "react";
 
+const Count = () => {
+  const [count, setCount] = useState(0)
+  function update() {
+    setTimeout(() => {
+      setCount(count + 1);
+    })
+    setTimeout(() => {
+      console.log(count)
+    })
+  }
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={update}>点击</button>
+    </>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className={classNames('sub-name', {'name-active': true})}>
+        <Count/>
       </header>
     </div>
   );
