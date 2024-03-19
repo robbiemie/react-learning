c0027ae41312bfa52cfbfa0b9da06eb4b8b51e60
You <xin.yang02@mihoyo.com>
2024-03-19 15:34:16 +0800

    feat: 编程式导航

--- src/page/login/index.js
+++ src/page/login/index.js
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom"
+
 function LoginPage() {
+  const navigate = useNavigate()
   return (<div>
     登录页
+    <button onClick={() => navigate('/article')}>查看详情</button>
   </div>)
 }
 
