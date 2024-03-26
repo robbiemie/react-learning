1f53b233951012da6ce5a90bb305f696be9006f8
robbiemie <robbie657850144@gmail.com>
2024-03-26 23:59:22 +0800

    feat: useReducer

--- src/page/board/index.js
+++ src/page/board/index.js
@@ -1,6 +1,27 @@
+// useReducer
+
+import { useReducer } from "react";
+
+// 1. 定义 reducer 函数
+function reducer(state, action) {
+  switch(action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DESC':
+      return state - 1;
+    default: return state
+  }
+}
+
+
+
 const Board = () => {
+  // 2.组件中使用useReducer
+  const [state, dispatch] = useReducer(reducer, 0)
   return (<div>
-    board
+    board: { state }<br/>
+    <button onClick={ () => dispatch({type: 'INC'})}>+1</button><br/>
+    <button onClick={ () => dispatch({type: 'DESC'})}>-1</button><br/>
   </div>)
 }
 
